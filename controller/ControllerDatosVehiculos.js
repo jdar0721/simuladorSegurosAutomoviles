3aea64ce85c99a2b761fb56c2daaf109eb8f003a
jdar0721 <jdar0721@gmail.com>
2024-11-14 23:25:24 -0500

    Se elabora en el controlador de datos de vehiculos la funcionalidad que al seleccionar una marca nos salga la lista desplegable de sus modelos

--- controller/ControllerDatosVehiculos.js
+++ controller/ControllerDatosVehiculos.js
@@ -1,11 +1,27 @@
-function actualizarModelos(){
-    const SelectModelos=document.getElementById('modelo')
-    SelectModelos.innerHTML=""
-}
-
-const modelos = {
-    'renault' : ['Duster', 'Logan', 'Kwid'],
-    'chevrolet' : ['Cruze', ]
-}
+function actualizarModelos(marcaSeleccionada) {
+    const selectModelo = document.getElementById('modelo');
+    selectModelo.innerHTML = ''; 
+  
+    const modelos = {
+      'renault': ['Duster', 'Logan', 'Kwid'],
+      'chevrolet': ['Cruze', 'Onix', 'Tracker'],
+      'nissan': ['Sentra', 'Versa', 'Altima']
+    };
+  
+    const modelosDisponibles = modelos[marcaSeleccionada] || [];
+  
+    modelosDisponibles.forEach(modelo => {
+      const option = document.createElement('option');
+      option.value = modelo;
+      option.text = modelo;
+      selectModelo.appendChild(option);
+    });
+  }
+  
+  const selectMarca = document.getElementById('marca'); 
+  selectMarca.addEventListener('change', () => {
+    const marcaSeleccionada = selectMarca.value;
+    actualizarModelos(marcaSeleccionada);
+  });
 
  
